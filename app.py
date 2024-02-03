9f70b60da484b2748cfbdefb1dc3b0f3bfa39aff
Habeeb Tijani <141710695+Mac-Eames@users.noreply.github.com>
2024-02-04 00:15:22 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+print ('create acount now')
+
+username = input('Enter username: ')
+password = input('Enter password: ')
+
+print('Account create successfully')
+print('log in now!')
+
+username2 = input('Enter username: ')
+password2 = input('Enter password: ')
+
+if username == username2 and password == password2:
+    print('Logged in successfully')
+else: 
+    print('Invalid Credentials')
